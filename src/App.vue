70be8eeb6c81aeb8436d2562935b6b54a8af06ec
AlekseyMohylnyi <alloexey@gmail.com>
2022-02-11 16:19:03 +0200

    friday add two

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <template>
   <div id="app">    
-    <!-- <Header /> -->
-    <!-- <What /> -->
+    <Header />
+    <What />
     <Contagion />
   </div>
 </template>
 
 <script>
-// import Header from './components/Header.vue'
-// import What from './components/What.vue'
+import Header from './components/Header.vue'
+import What from './components/What.vue'
 import Contagion from './components/Contagion.vue'
 
 export default {
   name: 'App',
   components: {
-    // Header,
-    // What,
+    Header,
+    What,
     Contagion
   }
 }
