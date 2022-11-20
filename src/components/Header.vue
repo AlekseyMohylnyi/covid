f2b2ecb6437867f3f2653ee62105854f73ae86dd
AlekseyMohylnyi <alloexey@gmail.com>
2022-11-20 11:20:08 +0200

    add year

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,16 @@
 <template>  
-    <header class="header">      
+    <header class="header">
       <div class="header__info">
         <img class="header__img" src="../assets/images/virus-one.svg" alt="img">
-        <h4 class="header__subtitle">COVID-22 Alert</h4>
+        <h4 class="header__subtitle">COVID-23 Alert</h4>
         <h1 class="header__title">Stay at Home quarantine <br/> To stop Corona virus</h1>
-        <p class="header__text">There is no specific medicine to prevent or treat coronavirus  disease (COVID-22). People may need supportive care to.</p>
+        <p class="header__text">There is no specific medicine to prevent or treat coronavirus disease (COVID-23). People may need supportive care to.</p>
         <a class="btn header__btn" href="#">Let Us Help</a>
       </div>
       <div class="header__img">
         <img src="../assets/images/header-bg.png" alt="img">
       </div>
-    </header>  
+    </header>
 </template>
 
 <script>
